141df8eab62986762541639d37223aecf979ba36
PrathamBorhade <prathamnb18@gmail.com>
2026-01-21 22:30:00 +0530

    Update Ass9_code5.py

--- Ass9_code5.py
+++ Ass9_code5.py
@@ -1,18 +1,5 @@
-def ChkDiv(Value):
-    if(Value!=3 and Value!=5):
-        return True
-    else:
-        return False
-
-def main():
-    print("Enter No : ")
-    No = int(input())
-    Result = ChkDiv(No)
-    if(Result==True):
-        print("Divisible by 3 and 5")
-    else:
-        print("Not divisible by 3 and 5")
-
-
-if __name__ == "__main__":
-    main()
+No = int(input("Enter a No : "))
+if(No % 3 == 0 and No % 5 == 0 ):
+    print("Divisible by 3 and 5 ")
+else:
+    print("Not divisible by both 3 and 5") 
