eccec23c51abdca2e2cea725aad1bb52a7c037ff
PrathamBorhade <prathamnb18@gmail.com>
2026-01-21 22:22:36 +0530

    Update Ass9_code4.py

--- Ass9_code4.py
+++ Ass9_code4.py
@@ -1,10 +1,2 @@
-def Cube(no1):
-    ans = 0
-    ans = no1**3
-    return ans
-
-Result = 0
 Num = int(input("Enter a no : "))
-
-Result = Cube(Num)
-print("Cube of the no is : ",Result)
+print("Cube of the no is : ",Num**3)
